c41cd04c84d078524568f480a65e959d238aa37e
Amit <amit26rawat1997@gmail.com>
2024-02-08 13:33:11 +0530

    Controller changes and adding UI

--- src/main/java/com/greatlearning/EmployeeManagementApplicationSecurity/Controller/EmployeeController.java
+++ src/main/java/com/greatlearning/EmployeeManagementApplicationSecurity/Controller/EmployeeController.java
@@ -3,11 +3,13 @@ package com.greatlearning.EmployeeManagementApplicationSecurity.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.greatlearning.EmployeeManagementApplicationSecurity.Entity.Employee;
 import com.greatlearning.EmployeeManagementApplicationSecurity.Service.EmployeeService;
@@ -15,36 +17,77 @@ import com.greatlearning.EmployeeManagementApplicationSecurity.Service.EmployeeS
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-@RestController
+@Controller
 public class EmployeeController {
 	
 	@Autowired
 	EmployeeService employeeService;
 	
-	@GetMapping("/employees")
-	public List<Employee> findAllEmployee(){
+	@GetMapping("/employees/employeeList")
+	public String findAllEmployee(Model model){
 		log.info("EmployeeController findAllEmployee()");
-		return employeeService.findAll();
+		List<Employee> employees = employeeService.findAll();
+		model.addAttribute("Employee", employees);
+		return "List-employee";
 	}
 	
-	@PostMapping("/saveEmployee")
-	public void saveEmployee(@RequestParam("id") int id,@RequestParam("firstName") String firstName,
-			@RequestParam("lastName") String lastName, @RequestParam("email") String email) {
-		
+	@GetMapping("/employee/showFormForAdd")
+	public String showFormForAdd(Model model) {
+		log.info("EmployeeController showFormForAdd()");
 		Employee newEmployee = new Employee();
-		newEmployee.setId(id);
-		newEmployee.setFirstName(firstName);
-		newEmployee.setLastName(lastName);
-		newEmployee.setEmail(email);
-		log.info("EmployeeController findAllEmployee()");
+		model.addAttribute("Employee", newEmployee);
 		employeeService.save(newEmployee);
+		return "Employee-form";
 	}
 	
-	@DeleteMapping("/deleteEmployeeById")
-	public void DeleteEmployeeById(Integer id)
+	@PostMapping("/employee/save")
+	public String save(@RequestParam("id") int id,@RequestParam("firstName") String firstName,
+			@RequestParam("lastName") String lastName,@RequestParam("email") String email) {
+		log.info("EmployeeController save()");
+		Employee newEmployee;
+		if(id!=0) {
+			newEmployee = employeeService.findById(id);
+			newEmployee.setFirstName(firstName);
+			newEmployee.setLastName(lastName);
+			newEmployee.setEmail(email);
+		}
+		else {
+			newEmployee = new Employee(id,firstName,lastName,email);
+		}
+		employeeService.save(newEmployee);
+		return "redirect:/employees/employeeList";
+	}
+	
+	@GetMapping("/employee/showFormForUpdate")
+	public String showFormForUpdate(@RequestParam("id") int id,Model model){
+		log.info("EmployeeController showFormForUpdate()");
+
+		Employee updatedEmployee = employeeService.findById(id);
+		model.addAttribute("employee", updatedEmployee);
+		return "Employee-form";
+	}
+	
+	@GetMapping("/employee/delete")
+	public String DeleteEmployeeById(@RequestParam("id") int id)
 	{
-		log.info("EmployeeController findAllEmployee()");
+		log.info("EmployeeController DeleteEmployeeById()");
 		employeeService.delete(id);
+		return "redirect:/employees/employeeList";
+	}
+	
+	@RequestMapping("/403")
+	public ModelAndView accessDenied(Principle user) {
+		log.info("EmployeeController accessDenied()");
+		ModelAndView model = new ModelAndView();
+		
+		if(user != null) {
+			model.addObject("msg","Hi "+user.getName()+", you don't have permission to this page!!!");
+		}
+		else {
+			model.addObject("msg", "you don't have permission to this page!!!");
+		}
+				
+		return model;
 	}
 
 }
--- src/main/java/com/greatlearning/EmployeeManagementApplicationSecurity/Entity/User.java
+++ src/main/java/com/greatlearning/EmployeeManagementApplicationSecurity/Entity/User.java
@@ -16,7 +16,6 @@ import javax.persistence.ManyToMany;
 
 import lombok.Data;
 
-@Data
 @Entity
 public class User {
 	
@@ -35,6 +34,38 @@ public class User {
 		joinColumns = @JoinColumn(name="user_id"),
 		inverseJoinColumns = @JoinColumn(name="role_id"))
 	private List<Role> roles = new ArrayList<>();
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public List<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(List<Role> roles) {
+		this.roles = roles;
+	}
 	
 	
 }
